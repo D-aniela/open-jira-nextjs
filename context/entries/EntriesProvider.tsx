d81f31784febe16b27cdd3e62e23b855c66673b6
Daniela Estrada <daniela.es.ib@gmail.com>
2023-07-24 20:58:13 -0600

    post method in new entry

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,10 +1,11 @@
-import { FC, useReducer } from 'react'
+import { FC, useEffect, useReducer } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import { Entry } from '@/interfaces'
 
 import { entriesReducer, EntriesContext } from './'
 import { NewEntry } from '../../components/ui/NewEntry'
+import { entriesApi } from '@/apis'
 
 export interface EntriesState {
   entries: Entry[]
@@ -37,13 +38,22 @@ export const EntriesProvider: FC = ({ children }) => {
     })
   }
 
+  const refreshEntries = async () => {
+    const { data } = await entriesApi.get<Entry[]>('/entries')
+    dispatch({ type: '[Entry] Refresh-Data', payload: data })
+  }
+
+  useEffect(() => {
+    refreshEntries()
+  }, [])
+
   return (
     <EntriesContext.Provider
       value={{
         ...state,
         // methods
         addNewEntry,
-        updateEntry
+        updateEntry,
       }}
     >
       {children}
--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -11,6 +11,10 @@ export default function handler(
   switch (req.method) {
     case 'GET':
       return getEntries(res)
+
+    case 'POST':
+      return postEntry(req, res)
+
     default:
       return res.status(400).json({ message: 'Endpoint no existe' })
   }
@@ -23,4 +27,22 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect()
 
   res.status(200).json(entries)
+}
+
+const postEntry = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { description = '' } = req.body
+
+  const newEntry = new Entry({ description, createdAt: Date.now() })
+
+  try {
+    await db.connect()
+    await newEntry.save()
+    await db.disconnect()
+
+    return res.status(201).json(newEntry)
+  } catch (error) {
+    await db.disconnect()
+    console.log('Error:', error);
+    return res.status(500).json({ message: 'Error al crear la entrada' })
+  }
 }
